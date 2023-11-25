42a3f3cda7c0119268b73f1e9dd0ac28c7757bce
Amanyadav257 <amanyadav01g@gmail.com>
2023-11-25 11:08:17 +0530

    firstCode

---
+++ Conditional.js
@@ -0,0 +1,31 @@
+let a=prompt("Hey enter- ");
+a=Number.parseInt(a);
+if(a > 0){
+    alert("this is a valid age");
+}
+else{
+    alert("this is invalid");
+}
+(a>10 && a<20) ? alert("this is a valid age") : alert("this is invalid");
+
+switch(a){
+    case 'b':
+        console.log("this is b");
+        break;
+    
+    case 'c':
+        console.log("this is c");
+        break;
+        
+    default:
+    console.log('sorry invalid number')
+}
+
+
+
+
+
+
+
+
+
