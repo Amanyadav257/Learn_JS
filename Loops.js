e97fe49261b1610bdff4f813a288ea530e0e958e
Amanyadav257 <amanyadav01g@gmail.com>
2023-11-25 11:40:42 +0530

    Loops

---
+++ Loops.js
@@ -0,0 +1,32 @@
+//Loops
+for(let a=0;a<10;a++){
+    console.log(a);
+}
+
+const obj={
+    harry: 30,
+    aman: 23,
+    rahul:80
+}
+console.log(obj.aman);
+for(let a in obj){
+    console.log(a + ":" + obj.a);
+}
+
+for(let a of "harry"){
+    if(a == 'y'){
+    console.log(a);
+  }
+}
+
+//while loop
+let a=0;
+while(a < 10){
+    console.log(a);
+    a++;
+}
+
+//do while
+do{
+    console.log(a);
+}while(a == 0);
