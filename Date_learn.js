fd61bcca4f9414190e8caaf68ce86c19a708c3cb
Amanyadav257 <amanyadav01g@gmail.com>
2023-11-27 19:11:44 +0530

    BOM

---
+++ Date_learn.js
@@ -0,0 +1,18 @@
+// var ty_d=new Date();
+// console.log(ty_d);
+// console.log(ty_d.getDate());
+
+
+// ty_d.setDate(12);
+// console.log(ty_d.getDate());
+// console.log(ty_d);
+
+var a=new Number(12.3);
+console.log(a);
+
+var b=12e4;
+let c=Number.MIN_VALUE;
+console.log(c);
+
+let d=Number.parseFloat("12.5");
+console.log(typeof(d));
